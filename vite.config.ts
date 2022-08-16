aa5c8ff1f9a039ceac8b5a66fb3e40792643e085
Viktor Zhurbin <vzhurbin@gmail.com>
2022-08-16 14:19:02 +0200

    config css modules

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,11 @@ export default defineConfig(() => {
         },
       },
     },
+    css: {
+      modules: {
+        localsConvention: "camelCaseOnly",
+      },
+    },
   };
   return config;
 });
